202cbb8de4d0462c0fdd1ba62ede95414a0d0c75
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-06-27 00:07:30 +0530

    ep-1

---
+++ Ep -1/script.js
@@ -0,0 +1,6 @@
+
+const parent=React.createElement("div",{id:"info"},
+    [React.createElement("h1",{},"My name is Vaidy dandriyal."),React.createElement("h1",{},"I am 20 years old.")]
+)
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
